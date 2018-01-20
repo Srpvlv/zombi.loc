395560fe6dd6cb3a24eb49c17cee317816d72ef2
Srpvlv <34565326+Srpvlv@users.noreply.github.com>
2018-01-20 14:42:56 +0300

    url setting

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'adasdf6asd5668adff7ft29',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +43,13 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+     
     ],
     'params' => $params,
 ];
